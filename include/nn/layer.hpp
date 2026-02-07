effbfc13afd2979e41cc9539b8aaddbb15005fa1
Yashwanth Madan <z5481579@ad.unsw.edu.au>
2026-02-07 15:51:11 +1100

    layer.hpp fixed

--- include/nn/layer.hpp
+++ include/nn/layer.hpp
@@ -4,7 +4,7 @@
 class Layer {
 public:
   virtual Eigen::MatrixXd forward(const Eigen::MatrixXd& X) = 0;
-  virtual Eigen::MatrixXd delete(const Eigen::MatrixXd& Y) = 0;
+  virtual Eigen::MatrixXd backward(const Eigen::MatrixXd& dY) = 0;
   virtual void update(double lr);
   virtual ~Layer() = default;
 };
